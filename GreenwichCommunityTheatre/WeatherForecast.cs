cf6b7a73394b77025c5e171ad65f1a9165b6a11a
shopeyinwale <shopeyinwale@gmail.com>
2025-02-01 11:29:17 +0000

    weather forecast model update

--- GreenwichCommunityTheatre/WeatherForecast.cs
+++ GreenwichCommunityTheatre/WeatherForecast.cs
@@ -8,7 +8,6 @@ namespace GreenwichCommunityTheatre
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-
-        //public string? Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
