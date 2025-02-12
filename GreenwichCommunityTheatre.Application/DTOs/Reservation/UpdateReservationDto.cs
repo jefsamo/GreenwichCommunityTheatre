dd7fbaf7b02a84c24f9c4890b5a04b8986dcbe76
shopeyinwale <shopeyinwale@gmail.com>
2025-02-12 00:38:07 +0000

    set reservation to paid implementation

--- GreenwichCommunityTheatre.Application/DTOs/Reservation/UpdateReservationDto.cs
+++ GreenwichCommunityTheatre.Application/DTOs/Reservation/UpdateReservationDto.cs
@@ -5,11 +5,6 @@ namespace GreenwichCommunityTheatre.Application.DTOs.Reservation
 {
     public class UpdateReservationDto
     {
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required Guid UserId { get; set; }
-        public required string Email { get; set; }
-        public ShippingOption ShippingOption { get; set; } = ShippingOption.Pickup;
-        public List<Ticket> Tickets { get; set; }
+        public required bool HasPaid { get; set; }
     }
 }
